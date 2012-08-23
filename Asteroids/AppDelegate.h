35e3d9bb4cae279a62ff658ab54f6f893b7f5a35
zjj <jj.weber@gmail.com>
2012-08-23 12:09:45 -0700

    Initial Commit

---
+++ Asteroids/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Asteroids
+//
+//  Created by JJ Weber on 8/23/12.
+//  Copyright (c) 2012 JJ Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Asteroids/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Asteroids
+//
+//  Created by JJ Weber on 8/23/12.
+//  Copyright (c) 2012 JJ Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
